98d7e957147e7aaf5b836ba8e9f25b9cdc358b91
Kei Oguro <kei.oguro@thebugmakers.com>
2024-12-05 09:36:11 +0900

    Add small code to show that reference value cannot be assigned to stackalloc-ed variable

---
+++ assign-reference-to-stackalloc-variable/Program.cs
@@ -0,0 +1,8 @@
+﻿/* Can not compile this.
+Span<string> s = stackalloc string[2] { null, null };
+Console.WriteLine($"{s.Length}, {s[0]}, {s[1]}");
+*/
+
+Span<int> i = stackalloc int[2] { 42, 777 };
+Console.WriteLine($"{i.Length}, {i[0]}, {i[1]}");
+
