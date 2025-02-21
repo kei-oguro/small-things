49bc4b025a4519794ad38fed4b6b834f1c549df2
Kei Oguro <kei.oguro@thebugmakers.com>
2025-02-22 02:20:53 +0900

    Add usage confirmation code for C# relational patterns.

---
+++ RelationalPatternsUsage/Program.cs
@@ -0,0 +1,5 @@
+﻿var x = 0;
+Console.WriteLine(x is < -1 or >= 1);
+// Console.WriteLine(x is < -1 and >= 1); // Compiler report this as error because an integer never match this pattern. That's inteligent.
+Console.WriteLine(x is < 2 and >= 1);
+Console.WriteLine($"{x}" is "cat" or "dog");
