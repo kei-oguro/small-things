8d7dce890739132a9a7a61ce06337a07725ce610
Kei Oguro <kei.oguro@thebugmakers.com>
2025-04-03 11:58:52 +0900

    Add confirmation code for creating/deleting (non-)existing directory.

---
+++ confirm-DirectoryInfo-usage/ConfirmDirectoryInfoUsage.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+
+public class ConfirmDirectoryInfoUsage
+{
+    private const string TargetPath = @"Folder/Sub";
+
+    public static void Main()
+    {
+        DirectoryInfo di = new(Path.Combine(Environment.CurrentDirectory, TargetPath));
+        Try(() => di.Delete(), "Deleting non-existing(?) directory"); // can throw DirectoryNotFoundException.
+        Try(() => di.Create(), "Creating a directory");
+        Try(() => di.Create(), "Creating a directory already exists"); // Doesn't throw.
+        Try(() => di.Delete(), "Deleting a directory");
+        Try(() => di.Delete(), "Deleting exactly non-existing directory"); // Throws DirectoryNotFoundException.
+
+        static void Try(Action action, string description)
+        {
+            try
+            {
+                action();
+                Console.WriteLine($"<{description}> succeeded.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"<{description}> throws <{e.GetType().Name}>: {e.Message}");
+            }
+        }
+    }
+}
