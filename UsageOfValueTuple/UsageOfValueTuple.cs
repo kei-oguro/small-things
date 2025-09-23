af8f6764ab56fc5309f294f7591cf963d86c399d
Kei Oguro <masuongm@gmail.com>
2025-09-23 19:04:33 +0900

    Add to confirm assignment for deconstructed variables.
    And can not deconstruct a single element ValueTuple.

---
+++ UsageOfValueTuple/UsageOfValueTuple.cs
@@ -0,0 +1,13 @@
+﻿// This shows what I was misunderstanding about assignment to a ValueTuple.
+// I thought that any variable decleared by deconstruction could not be assigned to.
+// But actually, the only things that could not be assigned to was a foreach iteration variable.
+
+var (a, b) = (1, 2);
+// var (c) = (3); // NG, I was wrong. I thought this declares a single element ValueTuple<int>, but it does not.
+var (c, _) = (3, 4);
+b = a + c; // OK, can be compiled. I was wrong.
+foreach (var (x, y) in new[] { (5, 6), (7, 8) })
+{
+    b += x + y;
+    // x = y; // NG
+}
